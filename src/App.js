e058e6218192e7be7d74763c2f6c1a0a3170d260
pronit <pronit78@gmail.com>
2017-09-27 20:00:36 +0530

    partial

--- src/App.js
+++ src/App.js
@@ -2,19 +2,20 @@ import React, { Component } from 'react';
 import './App.css';
 import ReactDataGrid from 'react-data-grid';
 import * as _ from 'lodash';
-import 'bootstrap/dist/css/bootstrap.css';
+let data = require("./data.json");
 
-var data = require("./data.json");
-
+const frozenColumns = [];
 class App extends Component {
 
     constructor(props){
         super(props);
         this.state= {
-            row:[],
+            row:data,
             column:[]
         };
         this.rowGetter =this.rowGetter.bind(this);
+        this.handleGridRowsUpdated = this.handleGridRowsUpdated.bind(this);
+        this.handleGridSort = this.handleGridSort.bind(this);
     }
 
 
@@ -28,23 +29,65 @@ class App extends Component {
         let keys = _.keys(data[0]);
         let columns =[];
         _.forEach(keys , (k)=>{
-          columns.push({"key": k ,"name":_.startCase(k), "resizable": true});
+            let columnDef = {key: k ,name:_.startCase(k),sortable: true, resizable: true ,editable:true , filterable: true ,cellClass:"no-overflow"};
+            if(_.includes(frozenColumns,k)){
+                columnDef = _.assign(columnDef , {locked: true});
+                columns.unshift(columnDef);
+            } else if ("isActive" === k){
+                console.log("do nothing");
+            } else {
+                columns.push(columnDef);
+            }
+            console.log(columnDef);
         });
         this.setState({column:columns});
     }
 
     rowGetter(i) {
-        return data[i];
+        const { row } = this.state;
+        if(row){
+            return row[i];
+        } else {
+            return data[i];
+        }
+
+    }
+
+    handleGridRowsUpdated({ fromRow, toRow, updated }) {
+        const {row} = this.state;
+        let rows = row.slice();
+
+        console.log("updaete");
+    }
+
+    handleGridSort(sortColumn, sortDirection) {
+        console.log("sortColumn" , sortColumn);
+        console.log("sortDirection" , _.lowerCase(sortDirection));
+
+
+        let rows = sortDirection === 'NONE' ? data : _.orderBy(data, sortColumn , _.lowerCase(sortDirection));
+        if(sortDirection !== 'NONE'){
+            console.log("sorted",rows);
+        }
+        this.setState({ row:rows });
     }
 
     render() {
         return  (
-            <ReactDataGrid
-                columns={this.state.column}
-                rowGetter={this.rowGetter}
-                rowsCount={data.length}
-                minHeight={500}
-                minColumnWidth={120} />);
+            <div style={{marginLeft: "auto",marginRight: "auto", marginTop:"100px",maxWidth: "1500px",float: "none"}}>
+                <ReactDataGrid
+                    // rowRenderer={RowRenderer}
+                    onGridSort={this.handleGridSort}
+                    columns={this.state.column}
+                    rowGetter={this.rowGetter}
+                    rowsCount={this.state.row.length}
+                    minHeight={1000}
+                    minColumnWidth={50}
+                    // enableCellSelect={true}
+                    // onGridRowsUpdated={this.handleGridRowsUpdated}
+                />
+            </div>
+        );
     }
 
 }
